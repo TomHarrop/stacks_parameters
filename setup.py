f66090b149bccdf1d416135ec63b9be78d97f3a6
Tom Harrop <twharrop@gmail.com>
2017-11-24 15:34:22 +1300

    plot the read counts per file for diagnostics

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def readme():
 
 setup(
     name='stacks_parameters',
-    version='0.0.1',
+    version='0.0.2',
     description='Parameter optimization for Stacks',
     long_description=readme(),
     url='https://github.com/TomHarrop/stacks_parameters',
